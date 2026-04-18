6e278faa81ef8a597659f84ddf2674bae4ab32af
fatimah7643 <fatimah013@gmail.com>
2026-04-18 22:11:51 +0800

    Fix endpoint DELETE /authentications.

--- src/routes/authentications.js
+++ src/routes/authentications.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { login, refreshToken, logout } = require('../handlers/authenticationsHandler');
 const { validateLogin, validateRefreshToken } = require('../validators/authenticationsValidator');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', validateLogin, login);
 router.put('/', validateRefreshToken, refreshToken);
-router.delete('/', validateRefreshToken, logout);
+router.delete('/', authMiddleware, validateRefreshToken, logout);
 
 module.exports = router;
